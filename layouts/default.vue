4ff264aa320e53fd54c4d8fce81a6bfebcf711d2
Zain Wania <zain@wania.dev>
2022-09-10 10:02:55 -0400

    something

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,44 @@
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+</script>
+
 <template>
-  <main class="py-20 px-10 text-center">
+  <main>
+    <nav class="flex justify-between mx-5 my-3">
+      <div class="gap-2">
+        <a href="#">
+          About
+        </a>
+        <a href="#">
+          Store
+        </a>
+      </div>
+      <div flex justify="center" content="center" class="gap-2">
+        <a href="#">
+          Gmail
+        </a>
+        <a href="#">
+          Images
+        </a>
+        <Menu>
+          <MenuButton>
+            <div i-ic-baseline-apps bg-light-200 />
+          </MenuButton>
+          <MenuItems class="z-50">
+            <MenuItem>
+              <a href="#">
+                Account settings
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a href="#">
+                Documentation
+              </a>
+            </MenuItem>
+          </MenuItems>
+        </menu>
+      </div>
+    </nav>
     <slot />
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
-    </div>
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,8 @@
 <template>
-  <div>
-    <Logos mb-6 />
-    <Suspense>
-      <PageView />
-      <template #fallback>
-        <div op50 italic>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+  <div class="text-center">
+    <h1 class="text-32 mt-52 text-center">
+      Google
+    </h1>
+    <input class=" w-80% rounded-full px-4 py-2" type="text" placeholder="Search">
   </div>
 </template>
